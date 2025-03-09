14ee6d2c7cfd790a5587fcf23083bd37b2049dfe
K.A.G.Virul Eksan <viruleksan.official@gmail.com>
2025-03-09 10:15:09 +0530

    Create BusRouteController.java

---
+++ src/main/java/services/passengers/Controller/BusRouteController.java
@@ -0,0 +1,16 @@
+package services.passengers.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bus-route")
+public class BusRouteController {
+
+    @GetMapping
+    public String getRoute(@RequestParam String origin,@RequestParam String destination){
+        return BusRouteService.getBusRoute(origin, destination);
+    }
+}
