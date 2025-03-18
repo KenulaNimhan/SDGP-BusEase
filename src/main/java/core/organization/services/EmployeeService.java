23593e8d2008eb5ecaa9b724ff1e626f47d50039
KenulaNimhan <kenulanimhan@gmail.com>
2025-03-18 23:31:15 +0530

    Update EmployeeService.java

--- src/main/java/core/organization/services/EmployeeService.java
+++ src/main/java/core/organization/services/EmployeeService.java
@@ -4,6 +4,9 @@ import core.organization.models.Employee;
 import core.util.DatabaseConnector;
 import org.springframework.stereotype.Service;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 @Service
@@ -18,7 +21,16 @@ public class EmployeeService {
     public String addNewEmp(Employee emp) {
         try {
             emp.setEmployeeID(createEmpID());
-            return dbConnect.addEmployeeToDB(emp);
+            if (
+                    isValidName(emp.getFirstName())
+                    && isValidName(emp.getLastName())
+                    && isValidNIC(emp.getNic())
+                    && isValidYear(emp.getDateOfBirth())
+            ) {
+                return dbConnect.addEmployeeToDB(emp);
+            } else {
+                return "INVALID-DATA";
+            }
         } catch (Exception e) {
             return "ERROR";
         }
@@ -28,5 +40,43 @@ public class EmployeeService {
         int currentEmpCount = dbConnect.getEmpCount();
         int newEmpCount = currentEmpCount+1;
         return "EM-"+newEmpCount;
+    }
+
+    // VALIDATOR METHODS
+    /**
+     * Validates that the given name contains only letters (no numbers or special characters).
+     * @param name the input name
+     * @return true if valid, false otherwise
+     */
+    private boolean isValidName(String name){
+        return name.matches("[a-zA-Z]+");
+    }
+
+    /**
+     * Validates if the given date is in YYYY-MM-dd format.
+     * @param date the input date
+     * @return true id valid, false otherwise
+     */
+    private boolean isValidYear(String date){
+        try{
+            LocalDate parsedDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            int year = parsedDate.getYear();
+            return year >= 1920 && year <= 3000;
+        }
+        catch (DateTimeException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Validates NIC (National Identity Card) number format.
+     * Acceptable formats:
+     * -9 digits followed by 'V' or 'v' (eg: 123456789V)
+     * -12-digit number (eg:200512345678)
+     * @param nic the input NIC
+     * @return true if valid, false otherwise
+     */
+    private boolean isValidNIC(String nic){
+        return nic.matches("\\d{9}[Vv]") || nic.matches("\\d{12}");
     }
 }
