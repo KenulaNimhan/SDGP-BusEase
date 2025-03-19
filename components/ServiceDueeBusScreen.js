bea75370fcfe88b28788fb4bdb269ea57f6c8fa2
dunimal2 <dunimal123@gmail.com>
2025-03-19 14:53:04 +0530

    Update ServiceDueeBusScreen.js

--- components/ServiceDueeBusScreen.js
+++ components/ServiceDueeBusScreen.js
@@ -39,3 +39,97 @@ const ServiceDueBusScreen = ({ navigation }) => {
             setLoading(false);
         }
     };
+
+
+     // Set sample data for demo purposes
+     const setSampleData = () => {
+        const sampleBuses = [
+            {
+                id: '1',
+                busNumber: 'BUS-123',
+                brand: 'Toyota',
+                model: 'Coaster 2023',
+                currentMileage: '25,000 km',
+                dueKm: '500 km',
+                lastServiceDate: '2023-06-20',
+                nextServiceDate: '2023-09-20',
+                serviceType: 'Regular Maintenance',
+                estimatedCost: '$500',
+                serviceHistory: [
+                    { date: '2023-06-20', type: 'Oil Change', cost: '$150' },
+                    { date: '2023-03-15', type: 'Tire Rotation', cost: '$100' },
+                    { date: '2023-01-30', type: 'Brake Inspection', cost: '$200' }
+                ]
+            },
+            {
+                id: '2',
+                busNumber: 'BUS-456',
+                brand: 'Mercedes-Benz',
+                model: 'Tourismo 2022',
+                currentMileage: '32,000 km',
+                dueKm: '200 km',
+                lastServiceDate: '2023-05-15',
+                nextServiceDate: '2023-08-15',
+                serviceType: 'Major Service',
+                estimatedCost: '$800',
+                serviceHistory: [
+                    { date: '2023-05-15', type: 'Full Service', cost: '$600' },
+                    { date: '2023-02-10', type: 'Oil Change', cost: '$150' },
+                    { date: '2023-01-05', type: 'Tire Replacement', cost: '$400' }
+                ]
+            },
+            {
+                id: '3',
+                busNumber: 'BUS-789',
+                brand: 'Volvo',
+                model: 'B11R 2023',
+                currentMileage: '18,000 km',
+                dueKm: '1,000 km',
+                lastServiceDate: '2023-07-01',
+                nextServiceDate: '2023-10-01',
+                serviceType: 'Oil Change',
+                estimatedCost: '$200',
+                serviceHistory: [
+                    { date: '2023-07-01', type: 'Oil Change', cost: '$150' },
+                    { date: '2023-04-15', type: 'Brake Service', cost: '$300' },
+                    { date: '2023-02-01', type: 'General Inspection', cost: '$100' }
+                ]
+            },
+            {
+                id: '4',
+                busNumber: 'BUS-101',
+                brand: 'Scania',
+                model: 'Touring 2022',
+                currentMileage: '45,000 km',
+                dueKm: '100 km',
+                lastServiceDate: '2023-06-10',
+                nextServiceDate: '2023-09-10',
+                serviceType: 'Full Service',
+                estimatedCost: '$1,000',
+                serviceHistory: [
+                    { date: '2023-06-10', type: 'Full Service', cost: '$800' },
+                    { date: '2023-03-20', type: 'Oil Change', cost: '$150' },
+                    { date: '2023-01-15', type: 'Tire Rotation', cost: '$100' }
+                ]
+            },
+            {
+                id: '5',
+                busNumber: 'BUS-202',
+                brand: 'MAN',
+                model: "Lion's Coach 2023",
+                currentMileage: '22,000 km',
+                dueKm: '800 km',
+                lastServiceDate: '2023-07-15',
+                nextServiceDate: '2023-10-15',
+                serviceType: 'Regular Maintenance',
+                estimatedCost: '$400',
+                serviceHistory: [
+                    { date: '2023-07-15', type: 'Regular Service', cost: '$300' },
+                    { date: '2023-04-20', type: 'Oil Change', cost: '$150' },
+                    { date: '2023-02-10', type: 'Brake Inspection', cost: '$200' }
+                ]
+            }
+        ];
+        
+        setServiceDueBuses(sampleBuses);
+    };
