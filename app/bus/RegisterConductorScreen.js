0ccb46cfd01c5f2ceb0a7886f9aa5777e30bdca4
Donsana <donsanthush@gmail.com>
2025-03-19 21:57:20 +0530

    Register Conductor Screen Function to Validate Name

---
+++ app/bus/RegisterConductorScreen.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView, Alert } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import API from '../services/api';
+
+const RegisterConductorScreen = ({ navigation }) => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [mobile, setMobile] = useState('');
+  const [licenseId, setLicenseId] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  // Function to validate name
+  const validateName = (name) => {
+    return name.length >= 2 && /^[a-zA-Z\s]*$/.test(name);
+  };
+
+  
