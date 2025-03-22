77d34f8b1137077ad2981e9dad7f39233ef5fdfd
yoosuf-fahran <yoosuffahran2001@gmail.com>
2025-03-22 19:53:23 +0530

    Structure and default variables for uesrname change section

---
+++ app/UsernameChange.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+const UsernameChangeScreen = ({ navigation }) => {
+  const [currentUsername, setCurrentUsername] = useState('user123');
+  const [newUsername, setNewUsername] = useState('');
+
+  return (
+    
+  );
+};
+
+
+
+export default UsernameChangeScreen;
